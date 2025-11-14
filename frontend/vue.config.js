f4e416f2bb544c34a3f04b6f93ef9c6de48c298a
wenlishi <101853652+wenlishi@users.noreply.github.com>
2025-11-14 14:01:41 +0800

    Comment out vue.config.js configuration

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,25 +1,25 @@
-const path = require('path')
+// const path = require('path')
 
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './')
-      }
-    }
-  },
-  transpileDependencies: ['@dcloudio/uni-app'],
-  devServer: {
-    port: '8080',
-    disableHostCheck: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 指向你的后端服务
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/api'
-        }
-      }
-    }
-  }
-}
+// module.exports = {
+//   configureWebpack: {
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, './')
+//       }
+//     }
+//   },
+//   transpileDependencies: ['@dcloudio/uni-app'],
+//   devServer: {
+//     port: '8080',
+//     disableHostCheck: true,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8080', // 指向你的后端服务
+//         changeOrigin: true,
+//         pathRewrite: {
+//           '^/api': '/api'
+//         }
+//       }
+//     }
+//   }
+// }
