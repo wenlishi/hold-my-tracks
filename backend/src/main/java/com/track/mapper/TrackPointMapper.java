14ec4994c2f1298af037bae126a837861f9e9359
wenlishi <3197066040@qq.com>
2025-11-14 10:44:57 +0800

    删除文件

--- backend/src/main/java/com/track/mapper/TrackPointMapper.java
+++
@@ -1,9 +0,0 @@
-package com.track.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.track.entity.TrackPoint;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TrackPointMapper extends BaseMapper<TrackPoint> {
-}
